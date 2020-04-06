9cd2ee3464017feaf6936bb4a50c2ad6a4716501
Elisa Gutierrez <elisa.gutierrez@onsistems.com>
2020-04-06 18:06:49 +0200

    galery

--- vistas/portfolio.template.php
+++ vistas/portfolio.template.php
@@ -2,48 +2,48 @@
 
         <div class="row">
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="../galeria/images/park.jpg">
-                    <img src="../galeria/images/park.jpg" alt="Park">
+                <a class="lightbox" href="galeria/images/editorial/campa_10.jpg">
+                    <img src="galeria/images/editorial/campa_10.jpg" alt="campa_10">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="../galeria/images/bridge.jpg">
-                    <img src="../galeria/images/bridge.jpg" alt="Bridge">
+                <a class="lightbox" href="galeria/images/editorial/campa_11.jpg">
+                    <img src="galeria/images/editorial/campa_11.jpg" alt="campa_11">
                 </a>
             </div>
             <div class="col-sm-12 col-md-4">
-                <a class="lightbox" href="../galeria/images/tunnel.jpg">
-                    <img src="../galeria/images/tunnel.jpg" alt="Tunnel">
+                <a class="lightbox" href="galeria/images/editorial/campa_12.jpg">
+                    <img src="galeria/images/editorial/campa_12.jpg" alt="campa_12">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="../galeria/images/coast.jpg">
-                    <img src="../galeria/images/coast.jpg" alt="Coast">
+                <a class="lightbox" href="galeria/images/editorial/campa_13.jpg">
+                    <img src="galeria/images/editorial/campa_13.jpg" alt="campa_13">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="../galeria/images/rails.jpg">
-                    <img src="../galeria/images/rails.jpg" alt="Rails">
+                <a class="lightbox" href="galeria/images/editorial/cristiano.jpg">
+                    <img src="galeria/images/editorial/cristiano.jpg" alt="cristiano">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="../galeria/images/traffic.jpg">
-                    <img src="../galeria/images/traffic.jpg" alt="Traffic">
+                <a class="lightbox" href="galeria/images/editorial/diptico.jpg">
+                    <img src="galeria/images/editorial/diptico.jpg" alt="diptico">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="../galeria/images/rocks.jpg">
-                    <img src="../galeria/images/rocks.jpg" alt="Rocks">
+                <a class="lightbox" href="galeria/images/editorial/libro_equilibrio.jpg">
+                    <img src="galeria/images/editorial/libro_equilibrio.jpg" alt="libro_equilibrio">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="../galeria/images/benches.jpg">
-                    <img src="../galeria/images/benches.jpg" alt="Benches">
+                <a class="lightbox" href="galeria/images/editorial/libro_tension.jpg">
+                    <img src="galeria/images/editorial/libro_tension.jpg" alt="libro_tension">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="../galeria/images/sky.jpg">
-                    <img src="../galeria/images/sky.jpg" alt="Sky">
+                <a class="lightbox" href="galeria/images/editorial/port_libro.jpg">
+                    <img src="galeria/images/editorial/port_libro.jpg" alt="port_libro">
                 </a>
             </div>
         </div>
